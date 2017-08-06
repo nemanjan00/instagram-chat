ca85abd671c7533346af9ee0c8d6bdf74b4f9607
nemanjan00 <nemanjan00@gmail.com>
2017-08-06 17:58:40 +0200

    Cleanup

--- frontend/src/controllers/login.js
+++ frontend/src/controllers/login.js
@@ -2,6 +2,8 @@ module.exports = function(app) {
 	app.controller("LoginController", function($scope) {
 		$scope.login = function(){
 			console.log($scope.username);
+			console.log($scope.password);
+			console.log($scope.remember);
 		};
 	});
 }
