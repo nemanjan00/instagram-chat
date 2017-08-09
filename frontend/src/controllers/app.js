b68467e8db705e98da1e6f8bbd6fb6e69e62ec6b
nemanjan00 <nemanjan00@gmail.com>
2017-08-09 15:18:07 +0200

    New version

--- frontend/src/controllers/app.js
+++ frontend/src/controllers/app.js
@@ -1,6 +1,10 @@
 module.exports = function(app) {
-	app.controller("AppController", function(user, $state) {
+	app.controller("AppController", function(user, $state, $scope) {
 		$(".splash").hide();
+
+		$scope.goToInbox = function(){
+			$state.go("app.inbox");
+		}
 
 		user.checkStatus().then(() => {
 			if(!user.isAuthenticated()){
